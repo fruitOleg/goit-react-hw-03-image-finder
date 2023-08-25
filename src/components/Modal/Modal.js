171d588f2ef008d8231164e9e2f1d1cda9b6a415
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-08-25 13:04:49 +0200

    upd

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,16 @@
-export const Modal = () => {
+import Modal from 'react-modal';
+import css from './Modal.module.css';
+Modal.setAppElement('#root');
+
+export const ModalWindow = ({ isModalOpen, imgModal, isModalClose }) => {
   return (
-    <div class="overlay">
-      <div class="modal">
-        <img src="" alt="" />
-      </div>
-    </div>
+    <Modal
+      isOpen={isModalOpen}
+      onRequestClose={isModalClose}
+      className={css.Modal}
+      overlayClassName={css.Overlay}
+    >
+      <img src={imgModal.largeImageURL} alt={imgModal.tags} />
+    </Modal>
   );
 };
